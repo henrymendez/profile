0480fb21aa2d26be99a4d2fdb5f7cce0398abc95
Henry Mendez <henry95@gmail.com>
2018-01-07 18:11:55 -0500

    Created setup scripts to make setting up my profile a lot easier.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Nothing to see here folks.
+# Profile
+My user profile across different operating systems, and small scripts I like.
+
+## Contains:
+* bash aliases & custom prompt (PS1)
+* color man pages
+* git settings & aliases
+* vimrc & vim plugins
+* tmux config with custom bindings (**Ctrl+x instead of Ctrl+b**)
+
+## Setup
+* Run the setup script depending on the operating system.
+```shell
+$ setup-scripts/bsd-setup.sh
+```
---
+++ setup-scripts/rpi-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# rpi-setup.sh
+#
+# Setup profile for raspbian
+
+shout() { echo "$0: $*" >&2; }
+barf() { shout "$*"; exit 100; }
+safe() { "$@" || barf "cannot $*"; }
+
+# Setup script working directory
+CWD="$( cd "$(dirname "$0")" ; pwd )/.."
+
+# bin scripts
+echo "Creating $HOME/bin ...\r\c"
+safe mkdir -pv "$HOME"/bin/
+echo "Creating $HOME/bin...done!\r"
+
+echo "Copying bin scripts...\r\c"
+safe cp "$CWD"/pi_bin/* "$HOME"/bin
+echo "Copying bin scripts...done!\r"
+
+# profile configs
+echo "Copying profile configs...\r\c"
+safe cp "$CWD"/configs/bash_aliases "$HOME"/.bash_aliases
+safe cp "$CWD"/configs/gitconfig "$HOME"/.gitconfig
+safe cp "$CWD"/configs/tmux.conf "$HOME"/.tmux.conf
+echo "Copying profile configs...done!\r"
+
+# vim stuff
+echo "Copying vim directory and config...\r\c"
+safe cp -r "$CWD"/vim/vim "$HOME"/.vim
+safe cp "$CWD"/vim/vimrc "$HOME"/.vimrc
+echo "Copying vim directory and config...done!\r"
+
+# End
+echo "Don't forget to run: git config --global user.email \"email@example.com\""
